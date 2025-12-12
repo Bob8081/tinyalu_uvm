f21630b03a0bedc419676b42c19ab8e7da3d3d02
shady abbas <144989308+shadyabbass@users.noreply.github.com>
2025-12-12 13:15:29 +0200

    Add files via upload

--- tb/mini_project_sequence.sv
+++ tb/mini_project_sequence.sv
@@ -9,14 +9,16 @@ super.new(name);
 endfunction
 
 // generation of a sequence os sequence_item
-virtual task body (sequence_item transaction);
-transaction= sequence_item::type_id::create("transaction");
+virtual task body ();
+sequence_item transaction
 
 repeat (100) begin
+transaction= sequence_item::type_id::create("transaction");
+
 strat_item (transaction); // wait until get_next_item from driver
 
 if (! transaction.randomize() )
-   `uvm_fatal("FATAL",$sformatf(" RANDOMIZATION IS FAILED !! "))
+   `uvm_fatal("FATAL",$sformatf(" RANDOMIZATION FAILED !! "))
 
 finish_item (transaction); // wait until item_done from driver
 end
