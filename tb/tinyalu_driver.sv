6667dcbc4d0592e2a02a2dc23d1f112a6470d905
Bob8081 <ombob456@gmail.com>
2025-12-13 06:20:25 +0200

    Fixed Problems with driver class & Propgating X's

--- tb/tinyalu_driver.sv
+++ tb/tinyalu_driver.sv
@@ -1,15 +1,15 @@
-class my_driver extends uvm_driver #(my_transaction);
+class tinyalu_driver extends uvm_driver #(tinyalu_seq_item);
 
     //declearations
-    my_transaction req;
+    tinyalu_seq_item req;
     virtual tinyalu_if vif;
 
     //registeration
-    `uvm_component_utils(my_driver)
+    `uvm_component_utils(tinyalu_driver)
 
 
     //construction
-    function new(string name = "my_driver", uvm_component parent = null);
+    function new(string name = "tinyalu_driver", uvm_component parent = null);
         super.new(name, parent);
     endfunction
 
@@ -28,18 +28,42 @@ class my_driver extends uvm_driver #(my_transaction);
 
     //run
     task run_phase(uvm_phase phase);
-        forever begin
-            wait(vif.driver_mp.reset_n)
-            seq_item_port.get_next_item(req);
-            @(vif.cb_drv)
 
+        // Initialize signals
+
+        vif.cb_drv.A     <= 0;
+        vif.cb_drv.B     <= 0;
+        vif.cb_drv.op    <= 3'b000;
+        vif.cb_drv.start <= 1'b0;
+
+
+        // `uvm_info("DRV", "I am driving", UVM_LOW)
+        wait(vif.driver_mp.reset_n === 1'b1);
+        // `uvm_info("DRV", "Reset done", UVM_LOW)   
+
+        forever begin
+            
+            seq_item_port.get_next_item(req);
+            // `uvm_info("DRV", "I am driving2", UVM_LOW)
+
+            @(vif.cb_drv)
             vif.cb_drv.A     <= req.A;
             vif.cb_drv.B     <= req.B;
             vif.cb_drv.op    <= req.op;
             vif.cb_drv.start <= 1'b1;
 
-            wait(vif.cb_drv.done == 1'b1);
-            vif.cb_drv.start <= 1'b0;
+            // `uvm_info("DRV", $sformatf("Driving A: 0x%0h, B: 0x%0h, OP: %0d", vif.cb_drv.A, vif.cb_drv.B, vif.cb_drv.op), UVM_LOW)
+
+            if (req.op == no_op) begin
+                // Special Handling for NOP
+                @(vif.cb_drv);
+                vif.cb_drv.start <= 1'b0;
+            end else begin
+                // Standard Handling (Add, Mul, etc):
+                wait(vif.cb_drv.done === 1'b1);
+                // De-assert start 
+                vif.cb_drv.start <= 1'b0;
+            end
 
             
             seq_item_port.item_done();
