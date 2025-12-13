116cc777ab1edffed1091f7e116f90eb9668db00
Bob8081 <ombob456@gmail.com>
2025-12-13 06:18:27 +0200

    Fixed Problems with driver class & Propgating X's

--- tb/tinyalu_monitor.sv
+++ tb/tinyalu_monitor.sv
@@ -1,47 +1,72 @@
-class tinyalu_monitor extends uvm_monitor;
-    `uvm_component_utils(tinyalu_monitor)
+class tinyalu_driver extends uvm_driver #(tinyalu_seq_item);
 
+    //declearations
+    tinyalu_seq_item req;
     virtual tinyalu_if vif;
-    
-    // Analysis Port to broadcast transactions
-    uvm_analysis_port #(tinyalu_seq_item) item_collected_port;
 
-    // Placeholder for the transaction being reconstructed
-    tinyalu_seq_item trans_collected;
+    //registeration
+    `uvm_component_utils(tinyalu_driver)
 
-    function new(string name, uvm_component parent);
+
+    //construction
+    function new(string name = "tinyalu_driver", uvm_component parent = null);
         super.new(name, parent);
-        trans_collected = new(); 
     endfunction
 
+    //build
     function void build_phase(uvm_phase phase);
         super.build_phase(phase);
-        item_collected_port = new("item_collected_port", this);
+
+        if(!uvm_config_db #(virtual tinyalu_if)::get(this, "", "vif", vif))
+            `uvm_fatal(get_type_name(), "Cannot get interface");
     endfunction
 
+    //connection
+    function void connect_phase(uvm_phase phase);
+        super.connect_phase(phase);
+    endfunction
+
+    //run
     task run_phase(uvm_phase phase);
-        // Wait for reset to be released
-        wait(vif.reset_n === 1);
+
+        // Initialize signals
+
+        vif.cb_drv.A     <= 0;
+        vif.cb_drv.B     <= 0;
+        vif.cb_drv.op    <= 3'b000;
+        vif.cb_drv.start <= 1'b0;
+
+
+        // `uvm_info("DRV", "I am driving", UVM_LOW)
+        wait(vif.driver_mp.reset_n === 1'b1);
+        // `uvm_info("DRV", "Reset done", UVM_LOW)   
 
         forever begin
-            // Wait for Start signal
-            // Ensures we only trigger when start goes high
-            @(vif.cb_mon iff vif.cb_mon.start === 1);
+            
+            seq_item_port.get_next_item(req);
+            // `uvm_info("DRV", "I am driving2", UVM_LOW)
 
-            // Sample Inputs (A, B, Op)
-            trans_collected.A   = vif.cb_mon.A;
-            trans_collected.B   = vif.cb_mon.B;
-            trans_collected.op  = op_t'(vif.cb_mon.op); // Cast to enum
+            @(vif.cb_drv)
+            vif.cb_drv.A     <= req.A;
+            vif.cb_drv.B     <= req.B;
+            vif.cb_drv.op    <= req.op;
+            vif.cb_drv.start <= 1'b1;
 
-            // Wait for Done signal
-            @(vif.cb_mon iff vif.cb_mon.done === 1);
+            // `uvm_info("DRV", $sformatf("Driving A: 0x%0h, B: 0x%0h, OP: %0d", vif.cb_drv.A, vif.cb_drv.B, vif.cb_drv.op), UVM_LOW)
 
-            // Sample Output (Result)
-            trans_collected.result = vif.cb_mon.result;
+            if (req.op == no_op) begin
+                // Special Handling for NOP
+                @(vif.cb_drv);
+                vif.cb_drv.start <= 1'b0;
+            end else begin
+                // Standard Handling (Add, Mul, etc):
+                wait(vif.cb_drv.done === 1'b1);
+                // De-assert start 
+                vif.cb_drv.start <= 1'b0;
+            end
 
-            // Broadcast to Scoreboard/Coverage
-            item_collected_port.write(trans_collected);
+            
+            seq_item_port.item_done();
         end
     endtask
-
 endclass
